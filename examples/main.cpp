cbeb422c540ce4f78f34ccb007b0db9f587f6b29
woXrooX <woXrooX@gmail.com>
2023-10-02 01:09:44 +0500

    External func usage example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,7 +1,16 @@
 #include "../source/server.hpp"
+#include <iostream>
+
+std::string resp = "HTTP/1.1 200 OK\r\nContent-Length: 12\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nHello World!";
+
+const std::string& handle_request(const std::string& data){
+	std::cout << data;
+	return resp;
+}
+
 
 int main(){
-    woXrooX::Server::start();
+    woXrooX::Server::init(handle_request, "127.0.0.1", "8008");
 
     return 1;
 }
