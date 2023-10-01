d087700447cd7767c0385aa0715e90eb79c45af6
woXrooX <woXrooX@gmail.com>
2023-10-01 15:32:35 +0500

    http parser on hold. moving to python interface

--- source/HTTP.hpp
+++ source/HTTP.hpp
@@ -3,184 +3,161 @@
 
 #include <iostream> // 4 debugging
 
+// Not sure if map is faster! map -> O(log n) - unordered_map ->  O(1) inbetween O(n)
+#include <unordered_map>
+
 namespace woXrooX{
-  class HTTP final{
-  public:
-    static void handle(const std::string& inData){
-      // Parse HTTP Request
-      HTTP::parse(inData);
+	class HTTP final{
+	public:
+		static void handle(const std::string& inData){
+			HTTP::RAW = inData;
 
-      // Handle GET
-      if(HTTP::requestMethod == "GET") HTTP::handle_GET();
+			// Parse HTTP Request
+			HTTP::parse();
 
-      // Update Response
-      HTTP::update_response();
-    }
+			// Handle GET
+			HTTP::responseStatus = Configurations::HTTP::ResponseStatus::ok;
+			HTTP::responseContentType = Configurations::HTTP::ContentType::textHTML;
+			HTTP::responseBody = "Hello world!";
 
-    //////// Getters
-    static const std::string& getRequestFirsLine(){return HTTP::requestFirstLine;}
-    static const std::string& getResponse(){return HTTP::response;}
-    static const std::string& getResponseFirstLine(){return HTTP::responseFirstLine;}
+			// Update Response
+			// HTTP::update_response();
+		}
 
-  private:
-    static inline void parse(const std::string& inData){
-      //// First Line
-      HTTP::requestFirstLine = inData.substr(0, inData.find("\r\n"));
+		//////// Getters
+		static const std::string& getFirsLine(){return HTTP::requestLine;}
+		static const std::string& getResponse(){return HTTP::response;}
+		static const std::string& getStatusLine(){return HTTP::responseStatusLine;}
 
-      // Validate First Line. If First Line Is Invalid Rest Doesn't Matter
-      int spaceCount = 0;
-      for(auto &c : HTTP::requestFirstLine) if(c == ' ') spaceCount++;
+	private:
+		static bool parse(){
 
-      if(spaceCount != 2){
-        Log::error("Invalid HTTP Request: Invalid First Line");
-        HTTP::hasError = true;
-        return;
-      }
-
-      // header
-      HTTP::requestHeader = inData.substr(0, inData.find("\r\n\r\n"));
-
-      // Method
-      HTTP::requestMethod = HTTP::requestFirstLine.substr(0, HTTP::requestFirstLine.find(' '));
-
-      // Protocol
-      HTTP::requestProtocol = HTTP::requestFirstLine.substr(
-        HTTP::requestFirstLine.rfind(' ')+1, HTTP::requestFirstLine.length() - HTTP::requestFirstLine.rfind(' ')
-      );
-
-      // File
-      HTTP::requestFile = HTTP::requestFirstLine.substr(
-        HTTP::requestFirstLine.find(' ')+1, HTTP::requestFirstLine.length() - HTTP::requestMethod.length() - HTTP::requestProtocol.length() - 2
-      );
-
-    }
-
-    static inline void handle_GET(){
-      //// Check If Request Is File Or Just Path
-      if(HTTP::requestFile.rfind('.') != std::string::npos && HTTP::requestFile.rfind('/') < HTTP::requestFile.rfind('.')){
-
-        //// Get The File
-        // 404
-        if(Files::getAll().find(HTTP::requestFile) == Files::getAll().end()){
-          HTTP::responseStatus = Configurations::HTTP::ResponseStatus::notFound;
-          HTTP::responseContentType.clear();
-          HTTP::responseBody.clear();
-        }
-
-        // 200
-        else{
-
-          //// If File Exists Then Define Response Content Type
-
-          // File Extension
-          std::string fileExtension = HTTP::requestFile.substr(HTTP::requestFile.rfind('.')+1, HTTP::requestFile.length() - HTTP::requestFile.rfind('.'));
-
-          // HTML
-          if(fileExtension == "html") HTTP::responseContentType = Configurations::HTTP::ContentType::textHTML;
-          // CSS
-          else if(fileExtension == "css") HTTP::responseContentType = Configurations::HTTP::ContentType::textCSS;
-          // JavaScript
-          else if(fileExtension == "js") HTTP::responseContentType = Configurations::HTTP::ContentType::textJavaScript;
-          // Text
-          else if(fileExtension == "txt") HTTP::responseContentType = Configurations::HTTP::ContentType::text;
-
-          // SVG
-          else if(fileExtension == "svg") HTTP::responseContentType = Configurations::HTTP::ContentType::imageSVG;
-          // ICO
-          else if(fileExtension == "ico") HTTP::responseContentType = Configurations::HTTP::ContentType::imageXIcon;
-          // PNG
-          else if(fileExtension == "png") HTTP::responseContentType = Configurations::HTTP::ContentType::imagePNG;
-          // JPEG & JPG
-          else if(fileExtension == "jpeg" || fileExtension == "jpg") HTTP::responseContentType = Configurations::HTTP::ContentType::imageJPEG;
-
-          // WOFF2
-          else if(fileExtension == "woff2") HTTP::responseContentType = Configurations::HTTP::ContentType::fontWOFF2;
-          // OTF
-          else if(fileExtension == "woff2") HTTP::responseContentType = Configurations::HTTP::ContentType::fontOTF;
-          // TTF
-          else if(fileExtension == "ttf") HTTP::responseContentType = Configurations::HTTP::ContentType::fontTTF;
-
-          // Unknown
-          else HTTP::responseContentType = "unknown/unknown";
+			// Check if the requestLine is valid
+			if(HTTP::parseRequestLine() == false) return false;
 
 
-          HTTP::responseStatus = Configurations::HTTP::ResponseStatus::ok;
-          HTTP::responseBody = Files::getAll().find(HTTP::requestFile)->second;
-        }
-      }
 
-      // Serve For Just Path Request
-      else{
-        HTTP::responseStatus = Configurations::HTTP::ResponseStatus::ok;
-        HTTP::responseContentType = Configurations::HTTP::ContentType::textHTML;
-        HTTP::responseBody = Files::getAll().find(Configurations::pathIndexHTML)->second;
-      }
-    }
+			// headers
+			// HTTP::requestHeader = inData.substr(0, inData.find("\r\n\r\n"));
 
-    static inline void update_response(){
-      HTTP::responseFirstLine = HTTP::responseProtocol + " " + HTTP::responseStatus;
-      HTTP::responseHeader =
-        HTTP::responseFirstLine + "\r\n" +
-        // HTTP::responseCacheControl + "\r\n" +
-        "Content-Type: " + HTTP::responseContentType + "\r\n" +
-        // "Content-Length: " + HTTP::responseBody::size() +
-        // HTTP::responseConnection +
-        HTTP::responseServer +
-        HTTP::responseHeaderEnd;
+			return true;
+		}
 
-      HTTP::response = HTTP::responseHeader + HTTP::responseBody;
+		static bool parseRequestLine(){
+			// Request Line
+			HTTP::requestLine = HTTP::RAW.substr(0, HTTP::RAW.find("\r\n"));
 
-    }
+			// Validate Request Line. If Request Line Is Invalid Rest Doesn't Matter
+			int spaceCount = 0;
+			for(auto &c : HTTP::requestLine) if(c == ' ') spaceCount++;
 
-    static bool hasError;
+			if(spaceCount != 2){
+				Log::error("Invalid HTTP Request: Invalid First Line");
+				HTTP::hasError = true;
+				return false;
+			}
 
-    // Request
-    static std::string requestHeader;
-    static std::string requestFirstLine;
-    static std::string requestMethod;
-    static std::string requestFile;
-    static std::string requestProtocol;
+			// Method
+			HTTP::requestMethod = HTTP::requestLine.substr(0, HTTP::requestLine.find(' '));
 
-    // Response
-    static std::string response;
-    static std::string responseHeader;
-    static std::string responseBody;
-    static std::string responseFirstLine;
-    static std::string responseStatus;
-    static std::string responseProtocol;
-    static std::string responseCacheControl;
-    static std::string responseContentType;
-    static std::string responseConnection;
-    static const std::string responseServer;
-    static const std::string responseHeaderEnd;
+			// Protocol
+			HTTP::requestProtocol = HTTP::requestLine.substr(
+				HTTP::requestLine.rfind(' ')+1, HTTP::requestLine.length() - HTTP::requestLine.rfind(' ')
+			);
 
-  };
+			// File
+			HTTP::requestFile = HTTP::requestLine.substr(
+				HTTP::requestLine.find(' ')+1, HTTP::requestLine.length() - HTTP::requestMethod.length() - HTTP::requestProtocol.length() - 2
+			);
 
-  bool HTTP::hasError = false;
+			return true;
+		}
 
-  ////// In Data
-  std::string HTTP::requestHeader;
-  std::string HTTP::requestFirstLine;
-  std::string HTTP::requestMethod;
-  std::string HTTP::requestFile;
-  std::string HTTP::requestProtocol;
+		static bool parseHeaders(){
+
+			return true;
+		}
+
+		static bool parseBody(){
+
+			return true;
+		}
+
+		static inline void update_response(){
+			HTTP::responseStatusLine = HTTP::responseProtocol + " " + HTTP::responseStatus;
+			HTTP::responseHeader =
+			HTTP::responseStatusLine + "\r\n" +
+			// HTTP::responseCacheControl + "\r\n" +
+			"Content-Type: " + HTTP::responseContentType + "\r\n" +
+			// "Content-Length: " + HTTP::responseBody::size() +
+			// HTTP::responseConnection +
+			HTTP::responseServer +
+			HTTP::responseHeaderEnd;
+
+			HTTP::response = HTTP::responseHeader + HTTP::responseBody;
+		}
+
+		static bool hasError;
+
+		static std::string RAW;
+
+		static std::string requestLine;
+		static std::string requestMethod;
+		static std::string requestFile;
+		static std::string requestProtocol;
 
 
-  ////// Out Data
-  std::string HTTP::response;
-  std::string HTTP::responseHeader;
-  std::string HTTP::responseBody;
-  std::string HTTP::responseFirstLine;
-  std::string HTTP::responseProtocol = "HTTP/2";
-  std::string HTTP::responseStatus;
-  std::string HTTP::responseCacheControl = "max-age=600";
-  std::string HTTP::responseContentType;
-  std::string HTTP::responseConnection = "Connection: Close\r\n";
-  const std::string HTTP::responseServer = "Server: woXrooX Web Server (WWS)\r\n";
-  const std::string HTTP::responseHeaderEnd = "\r\n";
+		static std::string statusLine;
 
+		static std::unordered_map<std::string, std::string> headers;
+		static std::string body;
+
+
+		// Request
+		static std::string requestHeader;
+		static std::string requestFirstLine;
+
+
+		// Response
+		static std::string response;
+		static std::string responseHeader;
+		static std::string responseBody;
+		static std::string responseStatusLine;
+		static std::string responseStatus;
+		static std::string responseProtocol;
+		static std::string responseCacheControl;
+		static std::string responseContentType;
+		static std::string responseConnection;
+		static const std::string responseServer;
+		static const std::string responseHeaderEnd;
+
+	};
+
+	bool HTTP::hasError = false;
+
+	std::string HTTP::RAW;
+	std::string HTTP::requestLine;
+	std::string HTTP::requestMethod;
+	std::string HTTP::requestFile;
+	std::string HTTP::requestProtocol;
+
+	////// In Data
+	std::string HTTP::requestHeader;
+	std::string HTTP::requestFirstLine;
+
+
+	////// Out Data
+	std::string HTTP::response;
+	std::string HTTP::responseHeader;
+	std::string HTTP::responseBody;
+	std::string HTTP::responseStatusLine;
+	std::string HTTP::responseProtocol = "HTTP/2";
+	std::string HTTP::responseStatus;
+	std::string HTTP::responseCacheControl = "max-age=600";
+	std::string HTTP::responseContentType;
+	std::string HTTP::responseConnection = "Connection: Close\r\n";
+	const std::string HTTP::responseServer = "Server: woXrooX Web Server (WWS)\r\n";
+	const std::string HTTP::responseHeaderEnd = "\r\n";
 }
 
 #endif
-
-// https://code.tutsplus.com/tutorials/http-headers-for-dummies--net-8039
