eb3a5b628ba1e97440f36742816ade0b1f2528aa
woXrooX <woXrooX@gmail.com>
2023-10-01 15:32:19 +0500

    conf back

--- source/configurations.hpp
+++ source/configurations.hpp
@@ -9,20 +9,15 @@ namespace woXrooX{
     /////// Server
     struct Server final{
       // "127.0.0.1" -> localhost
-      static inline const std::string IP = "192.168.0.194";
+      static inline const std::string IP = "127.0.0.1";
       static inline const std::string PORT = "8080";
-
-      // 4 GCP
-      // static inline const std::string IP = "10.166.0.2";
-      // static inline const std::string PORT = "80";
 
       static inline const int BUFFER = 131072;
       static inline const int BACKLOG = 1024;
-
     };
 
     /////// Http
-    struct Http final{
+    struct HTTP final{
       struct ContentType final{
         static inline const std::string text = "text/plain; charset=utf-8";
         static inline const std::string textHTML = "text/html; charset=utf-8";
@@ -45,20 +40,14 @@ namespace woXrooX{
         static inline const std::string ok = "200 OK";
         static inline const std::string notFound = "404 Not Found";
       };
-
     };
 
     // If Two Or More "front" Named Folders Exist In Configurations::absolutePathFront Then It Will Leack System Files!!!
     static inline const std::string absolutePathFront = "/home/woxroox/Desktop/woXTools/source/tools/server/front/";
 
-    // Path 4 GCP
-    // static inline const std::string absolutePathFront = "/home/woxroox/woXTools/source/tools/server/front/";
-
     static inline const std::string pathIndexHTML = "/html/index.html";
     static inline const std::string absolutePathBack = "";
-
   };
-
 }
 
 #endif
